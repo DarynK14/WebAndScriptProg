1e9d15d51f741afd6dd74ebb842213daec3b495e
DarynK14 <daryn.king@ontariotechu.net>
2024-11-05 10:58:33 -0500

    css work

--- Assignment 2/routes/index.js
+++ Assignment 2/routes/index.js
@@ -10,28 +10,28 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { 
+  res.render('home', { 
     title: 'Home' 
   });
 });
 
 /* GET about me page. */
 router.get('/aboutme', function(req, res, next) {
-  res.render('index', { 
+  res.render('aboutme', { 
     title: 'About Me' 
   });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { 
+  res.render('projects', { 
     title: 'Projects' 
   });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { 
+  res.render('contact', { 
     title: 'Contact Us' 
   });
 });
